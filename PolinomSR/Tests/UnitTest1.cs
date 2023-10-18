542e6d6c4e5acc5e34d04de6b4197a7c7743d5c0
TheRomaks <niks_2004@bk.ru>
2023-10-18 16:49:19 +0300

    testsfix

--- PolinomSR/Tests/UnitTest1.cs
+++ PolinomSR/Tests/UnitTest1.cs
@@ -8,44 +8,42 @@ namespace Tests
         [TestMethod]
         public void UmnoweniePolynomials_ReturnsCorrectResult()
         {
-            Polinom p1 = new Polinom(new double[] { 3, 0, -2 }); // 3x^2 - 2
-            Polinom p2 = new Polinom(new double[] { 2, 1 }); // 2x + 1
+            Polinom p1 = new Polinom(new double[] { -2, 0, 3 }); // 3x^2 - 2
+            Polinom p2 = new Polinom(new double[] { 1, 2 }); // 2x + 1
 
-            Polinom result = p1 * p2; // (3x^2 - 2) * (2x + 1) = 6x^3 + x^2 - 4x - 2
-            CollectionAssert.AreEqual((dynamic)new Polinom(new double[] { 6, 1, -4, -2 }).Coefs, (dynamic)result.Coefs);
+            Polinom result = p1 * p2; // (3x^2 - 2) * (2x + 1) = 6x^3 + 3x^2 - 4x - 2
+            CollectionAssert.AreEqual(new Polinom(new double[] { -2, -4, 3, 6 }).Coefs, result.Coefs);
         }
 
         [TestMethod]
         public void RazdeleniePolynomials_ReturnsCorrectResult()
         {
-            Polinom p1 = new Polinom(new double[] { 4, -2, 3, 1 }); // 4x^3 - 2x^2 + 3x + 1
+            Polinom p1 = new Polinom(new double[] { 1, 3, -2, 4 }); // 4x^3 - 2x^2 + 3x + 1
             Polinom p2 = new Polinom(new double[] { 1, 1 }); // x + 1
 
             Polinom result = p1 / p2; // (4x^3 - 2x^2 + 3x + 1) / (x + 1) = 4x^2 - 6x + 9
-
-            Assert.AreEqual(new Polinom(new double[] { 4, -6, 9 }), result);
+            CollectionAssert.AreEqual(new Polinom(new double[] { 9, -6, 4 }).Coefs, result.Coefs);
         }
 
         [TestMethod]
         public void SloweniePolynomials_ReturnsCorrectResult()
         {
-            Polinom p1 = new Polinom(new double[] { 3, 4, 2 }); // 3x^2 + 4x + 2
-            Polinom p2 = new Polinom(new double[] { 2, 5, 1 }); // 2x^2 + 5x + 1
+            Polinom p1 = new Polinom(new double[] { 2, 4, 3 }); // 3x^2 + 4x + 2
+            Polinom p2 = new Polinom(new double[] { 1, 5, 2 }); // 2x^2 + 5x + 1
 
             Polinom result = p1 + p2; // (3x^2 + 4x + 2) + (2x^2 + 5x + 1) = 5x^2 + 9x + 3
-
-            Assert.AreEqual(new Polinom(new double[] { 5, 9, 3 }), result);
+            CollectionAssert.AreEqual(new Polinom(new double[] { 3, 9, 5 }).Coefs, result.Coefs);
         }
 
         [TestMethod]
         public void VichitaniePolynomials_ReturnsCorrectResult()
         {
-            Polinom p1 = new Polinom(new double[] { 5, 2, -1 }); // 5x^3 + 2x^2 - x
-            Polinom p2 = new Polinom(new double[] { 2, -1, 1 }); // 2x^3 - x^2 + x
+            Polinom p1 = new Polinom(new double[] { -1, 2, 5 }); // 5x^2 + 2x^1 - 1
+            Polinom p2 = new Polinom(new double[] { 1, -1, 2 }); // 2x^2 - x^1 + 1
 
-            Polinom result = p1 - p2; // (5x^3 + 2x^2 - x) - (2x^3 - x^2 + x) = 3x^3 + 3x^2 - 2x
+            Polinom result = p1 - p2; // (5x^2 + 2x^1 - 1) - (2x^2 - x^1 + 1) = 3x^2 + 3x^1 - 2
 
-            Assert.AreEqual(new Polinom(new double[] { 3, 3, -2 }), result);
+            CollectionAssert.AreEqual(new Polinom(new double[] { -2, 3, 3 }).Coefs, result.Coefs);
         }
     }
 }
